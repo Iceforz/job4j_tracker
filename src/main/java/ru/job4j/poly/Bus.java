40d498c1071d61543bc7977c0d0fccb05c5f946d
iceforz <iceforz3@gmail.com>
2020-08-20 18:58:28 +0300

    3. Интерфейс Input [#296512]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+        System.out.println("Автобус движется");
+    }
+
+    @Override
+    public void passengers(int number) {
+        System.out.println("В автобусе" + number + "пассажиров");
+    }
+
+    @Override
+    public int cost(int petrol) {
+        return petrol*37;
+    }
+}
