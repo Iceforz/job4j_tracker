41eefb27f1ea27fd10df65481123d0afd35c4ae3
iceforz <iceforz3@gmail.com>
2020-08-24 16:24:40 +0300

    7. Шаблон проектирования - Стратегия. [#296523]

---
+++ src/main/java/stragery/Square.java
@@ -0,0 +1,16 @@
+package stragery;
+
+public class Square implements Shape{
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return     " ______" + ln
+                + "|      |" + ln
+                + "|      |" + ln
+                + "|______|" + ln;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Square().draw());
+    }
+}
