219a8b62c25bd86433c2e298afe174dd9952addd
iceforz <iceforz3@gmail.com>
2020-08-26 15:01:03 +0300

    1.1. Тесты на ValidateInput. [#296534]

--- src/test/java/ru/job4j/tracker/StartUI2Test.java
+++ src/test/java/ru/job4j/tracker/StartUI2Test.java
@@ -117,6 +117,27 @@ public class StartUI2Test {
         new StartUI2(out).init(in, tracker, actions);
         assertThat(tracker.findAll(), is("New item"));
     }
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "1", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI2(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit Menu ===%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. === Exit Menu ===%n"
+                )
+        ));
+    }
 }
 
 
