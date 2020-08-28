6a9ba3c702a48e24c0bc2419462d1811acd07f2f
iceforz <iceforz3@gmail.com>
2020-08-28 20:51:57 +0300

    2. Идентичные тексты. [#296503]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String key: origin) {
+            check.add(key);
+        }
+        for (String key: text) {
+            if (check.contains(key) == false) {
+                rsl = false;
+                break;
+            }
+        }
+
+        return rsl;
+    }
+}
