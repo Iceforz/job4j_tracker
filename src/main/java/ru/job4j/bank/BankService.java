a21fa5423da8e00e4b3de6c81bb2b7b0bec40184
iceforz <iceforz3@gmail.com>
2020-08-29 13:58:34 +0300

    3. Банковские переводы. [#296499]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,59 @@
+package ru.job4j.bank;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+       users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (!users.containsValue(account.getRequisite())) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User success = null;
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                success = user;
+                break;
+            }
+        }
+        return success;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account account = null;
+        User success = findByPassport(passport);
+        if (success != null) {
+            List<Account> req = users.get(success);
+            for (Account accounts : req) {
+                if (requisite.equals(accounts.getRequisite())) {
+                    account = accounts;
+                    break;
+                }
+            }
+        }
+        return account;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account tf = findByRequisite(srcPassport, srcRequisite);
+        Account sl = findByRequisite(destPassport, destRequisite);
+        if (amount <= tf.getBalance() || tf != null) {
+            tf.setBalance(tf.getBalance() - amount);
+            sl.setBalance(sl.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
