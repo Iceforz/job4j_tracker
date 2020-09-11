15dd6e249a6e4e58a88d390261fc9fd850731578
iceforz <iceforz3@gmail.com>
2020-09-11 14:06:02 +0300

    Отсортировать департаменты [#296480]

---
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartmentsTest {
+    @Test
+    public void whenMissed() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortUp() {
+        List<String> input = Arrays.asList("k1", "k1/sk1", "k2/sk2", "k2", "k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1",
+                "k2", "k2/sk2");
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenFillGapsAndSortDown() {
+        List<String> input = Arrays.asList("k2/sk2", "k1/sk1", "k2/sk2/ssk2");
+        List<String> expect = Arrays.asList("k2", "k2/sk2", "k2/sk2/ssk2", "k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortDesc(result);
+        assertThat(result, is(expect));
+    }
+}
