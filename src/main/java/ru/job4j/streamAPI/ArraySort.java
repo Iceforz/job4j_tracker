449ef074752af93457c77e7005398137daaef1ef
iceforz <iceforz3@gmail.com>
2020-09-15 15:36:19 +0300

    0. Stream API [#296350]

---
+++ src/main/java/ru/job4j/streamAPI/ArraySort.java
@@ -0,0 +1,20 @@
+package ru.job4j.streamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(-5);
+        list.add(-2);
+        List<Integer> bugs = list.stream().filter(
+                task -> task >= 0
+        ).collect(Collectors.toList());
+        bugs.forEach(System.out::println);
+    }
+}
+
