ccee8b851b498e5f8031b98a3759ad2b8f43dade
iceforz <iceforz3@gmail.com>
2020-09-15 14:49:19 +0300

    2.2. Лямбда блок [#296344]

--- src/main/java/ru/job4j/lambda/Lambda.java
+++ src/main/java/ru/job4j/lambda/Lambda.java
@@ -3,11 +3,13 @@ package ru.job4j.lambda;
 import java.util.Comparator;
 
 public class Lambda {
+    public static void main(String[] args) {
 
-    Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-    Comparator<String> cmpDescSize = (left, right) -> {
-        System.out.println("compare : " + right.length() + " - " + left.length());
-        return right.length() - left.length();
-    };
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare : " + right.length() + " - " + left.length());
+            return right.length() - left.length();
+        };
+    }
 }
 
