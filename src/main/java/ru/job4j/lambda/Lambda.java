862f6fbe02537352d0bd6be08cbbc1fc98568758
iceforz <iceforz3@gmail.com>
2020-09-15 14:37:41 +0300

    2.2. Лямбда блок [#296344]

---
+++ src/main/java/ru/job4j/lambda/Lambda.java
@@ -0,0 +1,13 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class Lambda {
+
+    Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+    Comparator<String> cmpDescSize = (left, right) -> {
+        System.out.println("compare : " + right.length() + " - " + left.length());
+        return right.length() - left.length();
+    };
+}
+
