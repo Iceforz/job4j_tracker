752ccd01d8aa66b70e3aed5ef25ebb78c104dfdc
iceforz <iceforz3@gmail.com>
2020-09-17 12:47:45 +0300

    1. Фильтрация учеников. [#296351]

---
+++ src/test/java/ru/job4j/lambda/SearchAttTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import junit.framework.TestCase;
+
+public class SearchAttTest extends TestCase {
+
+    public void testFilterSize() {
+    }
+
+    public void testFilterName() {
+    }
+
+    public void testFilter() {
+    }
+}
