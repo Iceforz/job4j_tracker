c872213b072c904ccf2251a743ff558ef43c055a
iceforz <iceforz3@gmail.com>
2020-09-21 15:39:40 +0300

    2. Local-Variable Type Inference [#296362]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -27,8 +27,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortUp() {
-        List<String> input = List.of("k1", "k1/sk1", "k2/sk2", "k2", "k1/sk1/ssk1");
-        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1",
+        List<String> input = Arrays.asList("k1", "k1/sk1", "k2/sk2", "k2", "k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1",
                 "k2", "k2/sk2");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
@@ -36,7 +36,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenFillGapsAndSortDown() {
-        List<String> input = Arrays.asList("k2/sk2", "k1/sk1", "k2/sk2/ssk2");
+        List<String> input = List.of("k2/sk2", "k1/sk1", "k2/sk2/ssk2");
         List<String> expect = Arrays.asList("k2", "k2/sk2", "k2/sk2/ssk2", "k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
